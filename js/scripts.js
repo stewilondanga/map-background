e9bd916f16dbdb1e97a36884873b9e8b6be97232
Stewart ilondanga <stilondanga@gmail.com>
2018-09-27 10:11:08 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,21 +9,21 @@ var navigate = (function() {
 
 var position = [-1.28333, 36.81667];
 
-/* function initialize() {
-	 var myOptions = {
-		 zoom: 10,
-		 streetViewControl: true,
-		 scaleControl: true,
-		 mapTypeId: google.maps.MapTypeId.ROADMAP
-	 };
-	 map = new google.maps.Map(document.getElementById('googlemaps'), myOptions);
-	 latLng = new google.maps.LatLng(position[0], position[1]);
-	 map.setCenter(latLng);
-	 marker = new google.maps.Marker({
-		 position: latLng,
-		 map: map,
-		 draggable: false,
-		 animation: google.maps.Animation.DROP
-	 });
- }
- google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+  var myOptions = {
+    zoom: 10,
+    streetViewControl: true,
+    scaleControl: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  /*	 map = new google.maps.Map(document.getElementById('googlemaps'), myOptions);
+  	 latLng = new google.maps.LatLng(position[0], position[1]);
+  	 map.setCenter(latLng);
+  	 marker = new google.maps.Marker({
+  		 position: latLng,
+  		 map: map,
+  		 draggable: false,
+  		 animation: google.maps.Animation.DROP
+  	 });
+   }
+   google.maps.event.addDomListener(window, 'load', initialize);
