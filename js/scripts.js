e81103063d5bc3097c258f3ebb5ea8533b108a6d
Stewart ilondanga <stilondanga@gmail.com>
2018-09-27 09:27:29 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,24 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var position = [-1.28333, 36.81667];
+
+ function initialize() {
+	 var myOptions = {
+		 zoom: 10,
+		 streetViewControl: true,
+		 scaleControl: true,
+		 mapTypeId: google.maps.MapTypeId.ROADMAP
+	 };
+	 map = new google.maps.Map(document.getElementById('googlemaps'), myOptions);
+	 latLng = new google.maps.LatLng(position[0], position[1]);
+	 map.setCenter(latLng);
+	 marker = new google.maps.Marker({
+		 position: latLng,
+		 map: map,
+		 draggable: false,
+		 animation: google.maps.Animation.DROP
+	 });
+ }
+ google.maps.event.addDomListener(window, 'load', initialize);
