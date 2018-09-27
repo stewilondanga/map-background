5ca80d049d074133e386662ada4a0a525e103ab4
Stewart ilondanga <stilondanga@gmail.com>
2018-09-27 10:11:43 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,14 @@ function initialize() {
     scaleControl: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  /*	 map = new google.maps.Map(document.getElementById('googlemaps'), myOptions);
-  	 latLng = new google.maps.LatLng(position[0], position[1]);
-  	 map.setCenter(latLng);
-  	 marker = new google.maps.Marker({
-  		 position: latLng,
-  		 map: map,
-  		 draggable: false,
-  		 animation: google.maps.Animation.DROP
-  	 });
-   }
-   google.maps.event.addDomListener(window, 'load', initialize);
+  map = new google.maps.Map(document.getElementById('googlemaps'), myOptions);
+  latLng = new google.maps.LatLng(position[0], position[1]);
+  map.setCenter(latLng);
+  marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    draggable: false,
+    animation: google.maps.Animation.DROP
+  });
+}
+/*   google.maps.event.addDomListener(window, 'load', initialize);
